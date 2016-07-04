ada651d4cf04a795b004e1d13026b76afccb1172
eladagam <elad.agam@gmail.com>
2016-07-04 14:59:27 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,15 @@
 """
-Write a program that randomizes integers in a loop
-until it finds a number that is divisable by: 7, 13 and 15
+Elad Agam
+05 - Rand numbers in loop (1-1000000) and stop only if it manage to rand a number that divides to 7, 13, and 15
 """
+
+from random import randint
+
+ 
+while True:
+	num = randint(1, 1000000)
+	if num % 7 == 0 and num % 13 == 0 and num % 15 == 0:
+		print "Found a number that divides to 7 13 15 => {n}".format(n=num)
+		break
+
 
