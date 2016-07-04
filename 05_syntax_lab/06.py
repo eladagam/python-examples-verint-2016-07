78b9061adee17b7e0fce99d3c558a0aa033c291f
eladagam <elad.agam@gmail.com>
2016-07-04 15:52:33 +0300

    Solution for: sol-python-verint.syntax-lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -1,9 +1,28 @@
 """
-Write a program that randomizes 2 numbers
-and calculates their least common multiplier,
-that is the smallest number that is divisable
-by both.
-For example if the numbers were 4 and 6,
-program should print 12
+Elad Agam
+06 - Rand two numbers (1-10) and find the LCM for both
 """
+
+from random import randint
+
+num1 = randint(1, 10)
+num2 = randint(1, 10)
+
+print "Randomized {n1} , {n2}".format(n1=num1,n2=num2)
+
+if num1 > num2:
+    greater = num1
+else:
+    greater = num2
+ 
+while True:
+    if greater % num1 == 0  and  greater % num2 == 0:
+	    lcm = greater
+	    break
+    greater = greater + 1
+
+print "LCM is {n1}".format(n1=lcm)
+
+
+
 
