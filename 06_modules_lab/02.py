fe2fed4dca4706aef2a5cabd8d7ea3626351d0a5
eladagam <elad.agam@gmail.com>
2016-07-07 21:14:08 +0300

    Solution for: sol-python-verint.modules-lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -6,4 +6,14 @@ Get command line arg x and y- print the sum of both
 import sys
 
 
-print "SUM: " + str(int(sys.argv[1])+int(sys.argv[2]))
+
+if (len(sys.argv) != 3):
+    print ('Error - need to send two args')
+    sys.exit()
+
+try: 
+    print "SUM: " + str(int(sys.argv[1])+int(sys.argv[2]))
+except ValueError:
+    print ('Error - need to send two numbers')
+    
+
