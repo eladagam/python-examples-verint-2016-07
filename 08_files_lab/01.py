023f66a0052a26dca17a760da582c68c78d278c5
eladagam <elad.agam@gmail.com>
2016-07-10 18:06:27 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,25 @@
 """
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
+Elad Agam - Files
+01 - Get two files and add the content of the first to the second
 """
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--source')
+parser.add_argument('-d', '--destination')
+args = vars(parser.parse_args())
+
+workingDir =  os.getcwd()
+
+source = os.path.join(workingDir, args['source'])
+destination = os.path.join(workingDir, args['destination'])
+
+
+with open(destination,"a") as fout:
+    with open(source,"r") as fin:
+        for line in fin:
+            fout.write(line)
+
 
