0b9865a6f7db315449733edeebbfd12f44eeecb2
eladagam <elad.agam@gmail.com>
2016-07-11 13:37:17 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,10 +1,32 @@
 """
-Write a program that takes two strings
-from the user and checks if they represent
-a valid user name.
-Valid users and passwords:
-    apple => red
-    lettuce => green
-    lemon => yellow
-    orange => orange
+Elad Agam - Data structures
+01 - Get a list of computer names as agrs
+     Look for the names in hosts file, and prin relevnt IP for each name
+     If not exists - print an error
 """
+
+
+import os
+import sys
+
+#create a Dictionary for hosts
+hosts_dictionary = {}
+
+#get computer names list from the user
+names=sys.argv[1:]
+
+#open hosts file and read its content to a dictionary
+workingDir =  os.getcwd()
+hosts = os.path.join(workingDir, "hosts")
+
+with open(hosts,"r") as fin:
+    for line in fin:
+        words = line.split("=")
+        hosts_dictionary[words[0]]=words[1]
+
+for name in names:
+    if name in hosts_dictionary:
+        print "IP of %s is %s" %(name , hosts_dictionary[name]),
+    else:
+        print "%s does not exist" %(name)
+
