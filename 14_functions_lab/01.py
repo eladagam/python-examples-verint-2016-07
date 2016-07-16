362665663a22cb90bcd71244ecd2f99878469533
eladagam <elad.agam@gmail.com>
2016-07-16 21:53:37 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,22 @@
 """
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
+Elad Agam - Functions
+01 - Create two functions Mul and sum
 """
 
+def mysum(*numbers):
+    sum=0
+    for num in numbers:
+        if not type(num) is str:
+            sum=sum+num
+    print sum
+
+mysum(12,123,"mkcdc")
+
+def mymul(*numbers):
+    mul=1
+    for num in numbers:
+        if not type(num) is str:
+            mul=mul*num
+    print mul
+
+mymul(12,3,2,"sxsd")
