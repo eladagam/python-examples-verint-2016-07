b7daaa471bcfc31714475bb43e4d712829475ce3
eladagam <elad.agam@gmail.com>
2016-07-16 22:00:58 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,12 @@
 """
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
+Elad Agam - Functions
+02 - Check input to function and raise exception if not correct
 """
 
+def f(number, name):
+        if  type(number) is str: raise Exception("Firts arg should be a number")
+        if not type(name) is str: raise Exception("Secont arg should be a string")
+        print "correct"
+
+
+f(1232,765656)
