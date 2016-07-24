a486e0a95eff0ead6d78fba9827274eb2e88ee79
eladagam <elad.agam@gmail.com>
2016-07-24 16:14:08 +0300

    Solution for: sol-python-verint.regexp-lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -1,6 +1,14 @@
 """
-Write a function toCamelCase(text) that returns the text camel cased
-Write a function to_underscore(text) that returns the text with 
-underscores between words
+Elad Agam - Regex
+02 - toCamelCase get string with '_' and replace to CamelCase
 """
 
+import re
+import argparse
+import os
+
+def toCamelCase(text):
+    str =  re.sub(r'_[a-z]', lambda m: m.group(0).upper(), text)
+    print re.sub(r'_','' , str)
+
+toCamelCase("no_more_shall_we_part")
