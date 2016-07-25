a1e0fead9e7d1a8cc282df8fc89c9db5e2ebd3de
eladagam <elad.agam@gmail.com>
2016-07-25 21:19:28 +0300

    Solution for: sol-python-verint.class-syntax-lab/03.py

--- 20_classes_lab/03.py
+++ 20_classes_lab/03.py
@@ -1,13 +1,45 @@
 """
-Following code assumes a class named Widget that depends on
-other widgets
-Widget ctor takes a name, and function 
-add_dependency(depends)
-marks widget as depends on all widgets in "depends" list
-
-When building a widget, you need to first build all the widgets it
-depends on.
-
-Implement widget so the following code works
+Elad Agam - Classes 
+03 - Build Class Widget
 """
 
+class Widget(object):
+    listOfBuilt=[]
+    def __init__(self,name):
+        self.wigName = name
+        self.dependList=[]
+        
+
+    #get list of Widget objects and add to list
+    def add_dependency(self, *dependencies):
+        self.dependList=dependencies
+
+    #Go over all object in dependencies list and call build
+    def build(self):
+        for d in self.dependList:
+            if d not in Widget.listOfBuilt:
+                
+                d.build()
+                print d.wigName
+                Widget.listOfBuilt.append(d)
+
+luke    = Widget("Luke")
+hansolo = Widget("Han Solo")
+leia    = Widget("Leia")
+yoda    = Widget("Yoda")
+padme   = Widget("Padme Amidala")
+anakin  = Widget("Anakin Skywalker")
+obi     = Widget("Obi-Wan")
+darth   = Widget("Darth Vader")
+_all    = Widget("All")
+
+
+luke.add_dependency(hansolo, leia, yoda)
+leia.add_dependency(padme, anakin)
+obi.add_dependency(yoda)
+darth.add_dependency(anakin)
+
+_all.add_dependency(luke, hansolo, leia, yoda, padme, anakin, obi, darth)
+_all.build()
+# code should print: Han Solo, Padme Amidala, Anakin Skywalker, Leia, Yoda, Luke, Obi-Wan, Darth Vader
+# (can print with newlines in between modules)
