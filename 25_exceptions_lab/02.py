bbfaa7be63375a8c13e6e8877d160ced61b10f58
eladagam <elad.agam@gmail.com>
2016-07-27 13:19:17 +0300

    Solution for: sol-python-verint.exceptions-lab/02.py

--- 25_exceptions_lab/02.py
+++ 25_exceptions_lab/02.py
@@ -1,7 +1,14 @@
 """
-Write a program that takes a file name
-and prints line count for the file
-
-Alert the user politely if there was any problem opening the file
+Elad Agam - Exceptions 
+02 - open file and print number of lines - assert if not exists 
 """
+import os
+import sys 
+
+
+if not os.path.isfile(sys.argv[1]):
+    raise ValueError("%s does not exist\n" % fin)
+
+with open(sys.argv[1],"r") as fin:
+    print sum(1 for _ in fin)
 
