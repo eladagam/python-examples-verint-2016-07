c4972098c19d4cffe8cb15807df5e68f6e983d24
eladagam <elad.agam@gmail.com>
2016-07-28 10:04:25 +0300

    Solution for: sol-python-verint.exceptions-lab/03.py

--- 25_exceptions_lab/03.py
+++ 25_exceptions_lab/03.py
@@ -1,4 +1,36 @@
 """
-Write code to make the following unit test pass
+Elad Agam - Exceptions 
+03 - Carete ImageFile Class that assert on wrong extension  
 """
 
+
+class InvalidImageExt(Exception):
+    def __init__(self, msg):
+        super(InvalidImageExt, self).__init__(msg)
+
+
+
+class ImageFile(object):
+    def __init__(self, name):
+        if not name.endswith(r'.png'):
+            raise(InvalidImageExt("wrong type of file"))
+
+
+
+
+
+
+import unittest
+
+class TestImageFile(unittest.TestCase):
+    def test_good_ext(self):
+        try:
+            img = ImageFile("file.png")
+        except InvalidImageExt:
+            self.fail("png should be a valid file extension")
+
+    def test_bad_ext(self):
+        with self.assertRaises(InvalidImageExt):
+            img = ImageFile("file.mp3")
+
+unittest.main()
